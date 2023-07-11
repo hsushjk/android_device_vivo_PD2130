5869e5a89204ed4c4f2358854c4703d3f4c8a258
Fuqing <47906326+hsushjk@users.noreply.github.com>
2023-07-11 20:06:41 +0800

    Update omni_PD2130.mk

--- omni_PD2130.mk
+++ omni_PD2130.mk
@@ -9,9 +9,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # Inherit from PD2130 device
 $(call inherit-product, device/vivo/PD2130/device.mk)
 
